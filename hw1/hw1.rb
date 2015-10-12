4e34b57b1fbd12398447f38458c15b04680f81bb
Joshua Burkhart Nyx <burkhart.joshua@gmail.com>
2015-10-11 21:31:53 -0700

    adding graphics.. sortof working

--- hw1/hw1.rb
+++ hw1/hw1.rb
@@ -1,6 +1,9 @@
 # Usage: ruby hw1.rb infile [outname]
 
 require_relative 'src/lib/vcf_parser'
+require 'mini_magick'
+require 'scruffy'
+require 'scruffy/rasterizers/mini_magick_rasterizer'
 
 infile = nil
 outname = nil
@@ -40,11 +43,38 @@ data_ary = vcf_parser.vcf_data
 data_ary.sort! {|a,b| a.position <=> b.position}
 min_data = data_ary.first
 max_data = data_ary.last
-
+begin
+graph = Scruffy::Graph.new
+graph.renderer = Scruffy::Renderers::Standard.new
+graph.value_formatter = Scruffy::Formatters::Percentage.new(:precision => 0)
+graph.add :line, 'Variant Position vs Alternate %', data_ary.collect{|line| line.alt_pct}
+graph.render :to => 'variant_pos_v_alt_pct.svg'
+graph.render :width => 800,
+             :height => 600,
+             :to => 'variant_pos_v_alt_pct.svg',
+             :as =>'png'
+rescue NoMethodError
+  puts $!
+  #scruffy gem sucks, has crappy docs, and should be updated
+end
 poly_count = data_ary.size
 data_ary.sort! {|a,b| a.alt_pct <=> b.alt_pct}
-rare_vars = data_ary.first(10)
-common_vars = data_ary.last(10)
+rare_vars = data_ary.first(100)
+common_vars = data_ary.last(100)
+begin
+  graph = Scruffy::Graph.new
+  graph.renderer = Scruffy::Renderers::Standard.new
+  graph.value_formatter = Scruffy::Formatters::Percentage.new(:precision => 0)
+  graph.add :bar, 'Variant Alternate % Top 100', data_ary.sort {|a,b| b.alt_pct <=> a.alt_pct}.collect{|line| line.alt_pct}.first(100)
+  graph.render :to => 'variant_alt_pct_100.svg'
+  graph.render :width => 800,
+               :height => 600,
+               :to => 'variant_alt_pct_100.svg',
+               :as =>'png'
+rescue NoMethodError
+  puts $!
+  #scruffy gem sucks, has crappy docs, and should be updated
+end
 metadata = vcf_parser.vcf_metadata
 summary_fptr = File.open(summary_filename,'w')
 summary_header = "Summary for #{infile}"
@@ -62,13 +92,13 @@ summary_fptr.puts("Polymorphic Site% (Polys/Refs): \
 #{format('%.4f',Float(poly_count * 100)/Float(max_data.position - min_data.position))}% \
 (#{poly_count}/#{max_data.position - min_data.position})")
 summary_fptr.puts
-summary_fptr.puts('Common Variants:')
+summary_fptr.puts('100 Most Common Variants:')
 summary_fptr.puts(" #{DataLine.header_s}")
 common_vars.reverse.each do |variant|
   summary_fptr.puts(" #{variant}")
 end
 summary_fptr.puts
-summary_fptr.puts('Rare Variants:')
+summary_fptr.puts('100 Rarest Variants:')
 summary_fptr.puts(" #{DataLine.header_s}")
 rare_vars.each do |variant|
   summary_fptr.puts(" #{variant}")
